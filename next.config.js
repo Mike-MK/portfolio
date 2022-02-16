994e01d493589bb3e2aeb45c081ebebca23d3b90
Mike-MK <michaelmukiri99@gmail.com>
2022-02-16 10:03:15 +0300

    change repo name

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
     };
   },
   //assetPrefix: '',
-  assetPrefix: !debug ? "/portfolio_website/" : "",
+  assetPrefix: !debug ? "/portfolio/" : "",
   webpack: (config, { dev }) => {
     // Perform customizations to webpack config
     // console.log('webpack');
